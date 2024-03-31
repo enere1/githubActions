ba9ae202f79fe946c5b71b2ce808d1bb0a9e1c04
enere1 <enere1@naver.com>
2024-03-31 19:46:19 +0900

    test追加

--- test/sum.test.js
+++ test/sum.test.js
@@ -10,3 +10,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 3 + 5 to equal 8', () => {
     expect(sum(3, 5)).toBe(8);
 });
+
+test('adds 3 + 10 to equal 13', () => {
+    expect(sum(3, 10)).toBe(13);
+});
