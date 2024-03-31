f64ff3431f2b8ab57981c474f45c23b2fe27bdd0
enere1 <enere1@naver.com>
2024-03-31 19:05:00 +0900

    init

---
+++ test/sum.test.js
@@ -0,0 +1,12 @@
+// sum.js
+function sum(a, b) {
+    return a + b;
+  }
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 + 5 to equal 8', () => {
+    expect(sum(3, 5)).toBe(8);
+});
